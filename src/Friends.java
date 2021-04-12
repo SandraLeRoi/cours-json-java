ea907b20492484280ef95c4d5e6faa5a475f1755
SandraLeRoi <oawx72@gmail.com>
2021-04-12 10:38:01 +0200

    debut correction recursivite

--- src/Friends.java
+++ src/Friends.java
@@ -3,6 +3,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -11,48 +12,99 @@ import java.util.stream.Collectors;
 public class Friends {
     static ArrayList<String> list;
     public static void main(String[] args) throws IOException {
-        String text = Files.readString(Paths.get("src/friends.json"));
-        System.out.println(JSONValue.parse(text));
-        JSONObject json = (JSONObject)JSONValue.parse(text);
-        JSONObject friens = json;
-        JSONArray friendsSarai = (JSONArray) friens.get("friends");
-        System.out.println(friendsSarai.size());
-        System.out.println(friendsSarai.stream().map(Friends::getName).collect(Collectors.toList()));
-
-
-       getFriends(friendsSarai);
+        String data = Files.readString(Paths.get("src/friends.json"));
+//        System.out.println(JSONValue.parse(data));
+        JSONObject sarai = (JSONObject)JSONValue.parse(data);
+        // question 1
+        System.out.println(getFriendsCount(sarai));
+        //question 2
+        System.out.println(getFriendsNames(sarai));
+        //question 3
+        System.out.println(getFriendsFriends(sarai));
+        // question 4
+        getAllNames(sarai);
+        // question 5
+        //getAllRelation(sarai, null);
+        getAllRelation2(sarai);
 
     }
 
-    static String getName(Object ami) {
-        JSONObject amiSarai = (JSONObject) ami;
-        return amiSarai.get("fname") + " " + amiSarai.get("lname") + amiSarai.get("friends");
+//    question 1
+    public static int getFriendsCount(JSONObject personne) {
+        JSONArray friends = (JSONArray) personne.get("friends");
+        return friends.size();
     }
 
-    static String getFriends (JSONArray amis) {
-        ArrayList<String> list = new ArrayList<>();
-
-        //Pour chaque ami de Sarai  je veux récupérer ses amis
-        for(Object ami :amis) {
-            JSONObject friendsOfFriends = (JSONObject) ami;
-            String lesPrenomAmis = (String) friendsOfFriends.get("fname");
-            String lesNomsAmis = (String) friendsOfFriends.get("lname");
-            String lesAmisAmis = (String) friendsOfFriends.get("friens");
-//            System.out.println(friendsOfFriends);
-            list.add(lesPrenomAmis + " " + lesNomsAmis + " " + lesAmisAmis);
-            System.out.println(list);
+    public static ArrayList<String> getFriendsNames(JSONObject person) {
+        JSONArray friends = (JSONArray) person.get("friends");
+        ArrayList<String> friendsNames = new ArrayList<>();
+        for (Object friendObject: friends) {
+            JSONObject friend = (JSONObject) friendObject;
+            String fname = (String) friend.get("fname");
+            String lname = (String) friend.get("lname");
+            friendsNames.add(fname+" "+lname);
         }
-        return String.valueOf(list);
-//        ArrayList<String> listeAmi = null;
-//        //Pour chaque ami de SArai je veux récupérer ses amis
-//        for(Object ami : amis) {
-//            JSONArray amiAmis = (JSONArray) ami;
-//            if (amiAmis.contains(ami)) {
-//                listeAmi.add(getFriends((JSONArray) ami));
-//            }
-//        }
-//
-//        return getName(listeAmi);
+        return friendsNames;
     }
 
+    public static ArrayList<String> getFriendsFriends(JSONObject person) {
+        JSONArray friends = (JSONArray) person.get("friends");
+        ArrayList<String> friendsFriendsNames = new ArrayList<>();
+        for (Object friendObject: friends) {
+            JSONObject friend = (JSONObject) friendObject;
+            ArrayList<String> names = getFriendsNames(friend);
+            friendsFriendsNames.addAll(names);
+        }
+        return friendsFriendsNames;
+    }
+
+    public static ArrayList<String> getAllNames(JSONObject person) {
+        String fname = (String) person.get("fname");
+        String lname = (String) person.get("lname");
+        System.out.println(fname+" "+lname);
+        JSONArray friends = (JSONArray) person.get("friends");
+        ArrayList<String> friendsFriendsNames = new ArrayList<>();
+        for (Object friendObject: friends) {
+            JSONObject friend = (JSONObject) friendObject;
+            getAllNames(friend);
+        }
+        return friendsFriendsNames;
+    }
+
+    public static void getAllRelation(JSONObject person, JSONObject parent) {
+        String fname = (String) person.get("fname");
+        String lname = (String) person.get("lname");
+        if(parent != null) {
+            String parentFname = (String) parent.get("fname");
+            String parentLname = (String) parent.get("lname");
+            System.out.println(parentFname+" "+parentLname+" - "+fname+" "+lname);
+        }
+        JSONArray friends = (JSONArray) person.get("friends");
+        for (Object friendObject: friends) {
+            JSONObject friend = (JSONObject) friendObject;
+            getAllRelation(friend, person);
+        }
+    }
+
+    public static void getAllRelation2(JSONObject person) {
+        JSONArray friends = (JSONArray) person.get("friends");
+        for (Object friendObject: friends) {
+            JSONObject friend = (JSONObject) friendObject;
+            getAllRelation2(friend, person);
+        }
+    }
+    public static void getAllRelation2(JSONObject person, JSONObject parent) {
+        String fname = (String) person.get("fname");
+        String lname = (String) person.get("lname");
+        if(parent != null) {
+            String parentFname = (String) parent.get("fname");
+            String parentLname = (String) parent.get("lname");
+            System.out.println(parentFname+" "+parentLname+" - "+fname+" "+lname);
+        }
+        JSONArray friends = (JSONArray) person.get("friends");
+        for (Object friendObject: friends) {
+            JSONObject friend = (JSONObject) friendObject;
+            getAllRelation2(friend, person);
+        }
+    }
 }
---
+++ src/exo_recursivite.md
@@ -0,0 +1,2 @@
+Afficher le nombre d'amis de Sarai Southwick
+Afficher le prénom et le nom de tous les amis de Sarai
